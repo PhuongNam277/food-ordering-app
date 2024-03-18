45aa9e7e4e1ab1d9e25d7b6afcf44827cd30971c
PhuongNam277 <namp3411@gmail.com>
2024-03-18 23:00:03 +0700

    Finished Hero.js and start create Home Menu

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,12 @@
+export default function HomeMenu() {
+    return (
+        <>
+          <div>
+            <h3>Check out</h3>
+            <h2>Menu</h2>
+
+          </div>
+
+        </>
+    );
+}
